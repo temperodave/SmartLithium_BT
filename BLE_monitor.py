c186c02b7b7dca9382b7992bb9b7251fa02b44fc
temperodave <dave@tempero.com>
2024-01-09 07:09:26 -0800

    Reduced debugging output

--- BLE_monitor.py
+++ BLE_monitor.py
@@ -142,10 +142,8 @@ def new_device_hndlr(proxy: BluezObjectManager,
     address = props.get('Address')
     ManufacturerData = props.get('ManufacturerData')
     if address in encryption_keys:
-        print(f'Device with address {address} FOUND in the list of known keys')
 #        logger.debug('New Device Handler: %s : %s', object_path, props)
         ManufacturerData_hex = ''.join(f'{i:02x}' for i in ManufacturerData[737])
-        print(f'Device with address {address} found. ManufacturerData in hex is:', ManufacturerData_hex, ' type ',type(ManufacturerData_hex))
 #              f'Removing from BlueZ cache')
 #        print(f'  ManufacturerData is {ManufacturerData[737]}  type:', type(ManufacturerData[737]))
 #        print(f'  ManufacturerData is ', ''.join(f'{i:02x}' for i in ManufacturerData[737]))
@@ -154,17 +152,17 @@ def new_device_hndlr(proxy: BluezObjectManager,
         length = len(ManufacturerData_hex)
         encrypted_hex=ManufacturerData_hex[16:length]
         encrypted=bytes.fromhex(ManufacturerData_hex[16:length])
-        print('  Encrypted_hex: ', encrypted_hex, ' encrypted ', encrypted)
+        print(f'Device with address {address} found. ManufacturerData in hex is:', ManufacturerData_hex, ' Encrypted_hex ',encrypted_hex)
         akey = bytes.fromhex(encryption_keys[address])
         # NOTE!!! IV bytes are flipped from the broadcast string and then the counter is decremented by 1 as the decrypt function increments before use
         iv = int(ManufacturerData_hex[12:14] + ManufacturerData_hex[10:12],16) - 1
-        print('  Raw IV is ', hex(iv), ' integer version is ', iv, ' ByteSwapped version is ', ManufacturerData_hex[12:14] + ManufacturerData_hex[10:12])
+#        print('  Raw IV is ', hex(iv), ' integer version is ', iv, ' ByteSwapped version is ', ManufacturerData_hex[12:14] + ManufacturerData_hex[10:12])
         print(f'      Encrypted Data is {encrypted.hex()} and key is {encryption_keys[address]} and iv is {iv}') 
         ciphertext_chunks = [encrypted]
         for plaintext in decrypt_aes_128_ctr_little_endian(akey,iv, ciphertext_chunks):
-            print('Decrypted text in hex: ', codecs.encode(plaintext,"hex"))
-    else:
-        print(f'Device with address {address} not in the list of known keys')
+            print('.     Decrypted text in hex: ', codecs.encode(plaintext,"hex"))
+#    else:
+#        print(f'Device with address {address} not in the list of known keys')
     adapter.RemoveDevice('(o)', object_path)
     
 
